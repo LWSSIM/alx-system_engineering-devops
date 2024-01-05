d36c11b875c88da8afbcb87afe47229bcb8f3a4a
LWSSIM <wassim.elmnajja2@gmail.com>
2024-01-05 15:30:06 +0100

    changed 6-*

--- 0x06-regular_expressions/6-phone_number.rb
+++ 0x06-regular_expressions/6-phone_number.rb
@@ -1,4 +1,4 @@
 #!/usr/bin/env ruby
 # Match exactly a 10 digit phone number
 # {\A Start of string + \d  Any digit + a{num}  Exactly num of a}
-puts ARGV[0].scan(/\A\d{10}/).join
+puts ARGV[0].scan(/^\d{10}$/).join
