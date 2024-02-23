381066cab6a7665f3a86a86d6812d95db65b22d9
LWSSIM <wassim.elmnajja2@gmail.com>
2024-02-23 18:26:25 +0100

    added 1- dependency insurance

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,15 @@
 # Installs Flask version-2.1.0 from pip3
 package { 'flask':
-  ensure   => '2.1.0',
+  ensure          => '2.1.0',
+  provider        => 'pip3',
+  install_options => ['--upgrade'],
+  require         => Package['python3-pip'],  # Ensure pip3 is installed first
+  unless          => 'pip3 show flask | grep -q "Werkzeug (2.1.1)"',  # Check if already installed
+  before          => Package['werkzeug'],  # Ensure Flask is installed before Werkzeug
+}
+
+package { 'werkzeug':
+  ensure   => '2.1.1',
   provider => 'pip3',
+  require  => Package['flask'],  # Ensure Flask is installed first
 }
