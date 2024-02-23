06594f6aab76caac1363781a8f2243a0464d9e33
LWSSIM <wassim.elmnajja2@gmail.com>
2024-02-23 18:40:00 +0100

    ensured dependencies

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,15 +1,16 @@
 # Installs Flask version-2.1.0 from pip3
+package { 'python3-pip':
+  ensure          => installed,
+}
+
 package { 'flask':
-  ensure          => '2.1.0',
-  provider        => 'pip3',
-  install_options => ['--upgrade'],
-  require         => Package['python3-pip'],  # Ensure pip3 is installed first
-  unless          => 'pip3 show flask | grep -q "Werkzeug (2.1.1)"',  # Check if already installed
-  before          => Package['werkzeug'],  # Ensure Flask is installed before Werkzeug
+  ensure   => '2.1.0',
+  provider => 'pip3',
+  require  => Package['python3-pip'],
 }
 
 package { 'werkzeug':
   ensure   => '2.1.1',
   provider => 'pip3',
-  require  => Package['flask'],  # Ensure Flask is installed first
+  require  => Package['flask'],
 }
