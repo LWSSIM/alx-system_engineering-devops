d0706cc065d9c1561760653618b9890baa1697c0
LWSSIM <wassim.elmnajja2@gmail.com>
2024-03-26 02:43:05 +0000

    added1-*

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+""" Gather data from an API
+addition to the first task, export data in the CSV format.
+"""
+
+
+import requests
+import sys
+import csv
+
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print('Usage: {} user_id'.format(sys.argv[0]))
+        exit(0)
+    if not sys.argv[1].isdigit():
+        print('User id must be an integer')
+        exit(0)
+    id = sys.argv[1]
+    url = 'https://jsonplaceholder.typicode.com/users'
+    request = requests.get(url)
+    if request.status_code != 200:
+        print('An error has occurred')
+        exit(0)
+    else:
+        response = request.json()
+
+    for user in response:
+        user_id = user.get('id')
+        if user_id != int(id):
+            continue
+        name = user.get('name')
+        request = requests.get(
+            'https://jsonplaceholder.typicode.com/todos?userId={}'
+            .format(user_id)
+        )
+        if request.status_code != 200:
+            print('An error has occurred')
+            exit(0)
+        todos = request.json()
+        with open(f'{user_id}.csv', 'w') as file:
+            writer = csv.writer(file, quoting=csv.QUOTE_ALL)
+            for task in todos:
+                writer.writerow(
+                    [user_id, name, task.get('completed'), task.get('title')]
+                )
