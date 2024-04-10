6158a529cb0c4255a7d11fb12ef277ebcc993a9f
LWSSIM <wassim.elmnajja2@gmail.com>
2024-04-10 21:21:21 +0000

    fix 1

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -7,12 +7,12 @@ import requests
 
 def top_ten(subreddit):
     """ top_ten function """
-    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
+    url = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
     headers = {'User-Agent': 'CustomAgent/5.0'}
-    response = requests.get(url, headers=headers)
+    response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 200:
-        for i in range(10):
-            print(response.json().get('data').get('children')[i]
-                  .get('data').get('title'))
+        data = response.json().get('data').get('children')
+        for post in data:
+            print(post.get('data').get('title'))
     else:
         print(None)
