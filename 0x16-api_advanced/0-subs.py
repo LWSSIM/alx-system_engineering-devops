f86df4030344b81f1a52dfaf81df69bb3164a461
LWSSIM <wassim.elmnajja2@gmail.com>
2024-04-10 21:13:39 +0000

    qdded

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,12 +8,11 @@ import requests
 
 def number_of_subscribers(subreddit):
     """ returns the number of subscribers
-    (not active users, total subscribers) for a given subreddit.
     """
-    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    headers = {'User-Agent': 'User-Agent-task/1.0'}
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-Agent": "User-Agent-task/1.0"}
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 200:
         data = response.json()
-        return data.get('data').get('subscribers')
+        return data["data"]["subscribers"]
     return 0
