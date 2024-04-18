e6000053f30e2bfe06270ec44543cdc4e3fac993
LWSSIM <wassim.elmnajja2@gmail.com>
2024-04-18 21:07:08 +0100

    added 0-*

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,20 @@
+# nginx_config.pp
+
+# Define the content of the /etc/default/nginx file
+file { '/etc/default/nginx':
+  ensure  => file,
+}
+
+exec { 'ulimit increase':}
+  command => "sed -i 's/^ULIMIT=.*/ULIMIT=\"n 10000\"/' /etc/default/nginx",
+  provider => 'shell',
+  require => File['/etc/default/nginx'],  # Ensure the package is installed after the file is created
+  notify  => Service['nginx'],  # Notify the NGINX service when the file changes
+}
+
+# Manage the NGINX service
+service { 'nginx':
+  ensure  => running,
+  enable  => true,
+  require => File['/etc/default/nginx'],  # Ensure the service restarts after the file changes
+}
