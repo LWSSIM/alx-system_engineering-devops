46d9e6d85c09c38b55e9d9c5a5d0f7110e26ae32
LWSSIM <wassim.elmnajja2@gmail.com>
2024-04-19 01:16:30 +0100

    added sed fix

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,11 +1,12 @@
 # Change user limit for open files
 
-exec {'hard_nofile':
-    command  => 'sed -i \"s/\(^holberton hard nofile \)[0-9]\+/\2048/\" /etc/security/limits.conf',
+exec { 'hard_nofile':
+    command  => "sed -i 's/\\(^holberton hard nofile \\)[0-9]\\+/\\12048/' /etc/security/limits.conf",
     provider => 'shell',
 }
 
-exec {'soft_nofile':
-    command  => 'sed -i \"s/\(^holberton soft nofile \)[0-9]\+/\1048/\" /etc/security/limits.conf',
+exec { 'soft_nofile':
+    command  => "sed -i 's/\\(^holberton soft nofile \\)[0-9]\\+/\\11048/' /etc/security/limits.conf",
     provider => 'shell',
 }
+
